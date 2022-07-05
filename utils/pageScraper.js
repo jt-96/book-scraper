ccf5632ed0b14696de5fa22f8ca84fbd14a02f9e
jt-96 <jetorrico@outlook.com>
2022-07-05 14:44:55 -0300

    feat(backend): get all links to books in stock

--- utils/pageScraper.js
+++ utils/pageScraper.js
@@ -1,9 +1,25 @@
 const scraperObject = {
     url: 'http://books.toscrape.com',
     async scraper(browser) {
+        //Create new page
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
+        //Navigate to selected url
         await page.goto(this.url);
+
+        //Wait for selector
+        await page.waitForSelector('.page_inner');
+        //Get the Link for all books
+        let urls = await page.$$eval('section ol > li', links => {
+            //check if books is in stock
+            links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== 'In stock');
+
+            //extract the links
+            links = links.map(el => el.querySelector('h3 > a').href)
+            return links;
+        })
+
+        console.log(urls);
     }
 }
 
